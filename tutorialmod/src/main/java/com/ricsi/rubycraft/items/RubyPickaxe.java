966ea9bffc604f5432a36c49e515f6a0c2bbf70b
RichardPalotai <jr.richard.palotai@gmail.com>
2020-06-23 17:23:54 +0200

    Added Ruby Pickaxe and Italian language.

--- tutorialmod/src/main/java/com/ricsi/rubycraft/items/RubyPickaxe.java
+++ tutorialmod/src/main/java/com/ricsi/rubycraft/items/RubyPickaxe.java
@@ -10,10 +10,8 @@ public class RubyPickaxe extends Item {
     public RubyPickaxe() {
         super(new Properties()
         .group(rubycraft.TAB)
-        .maxDamage(7)
         .defaultMaxDamage(6)
-        .addToolType(ToolType.PICKAXE, 10)
-        .maxStackSize(1)
+        .addToolType(ToolType.PICKAXE, 5)
         );
     }
 }
