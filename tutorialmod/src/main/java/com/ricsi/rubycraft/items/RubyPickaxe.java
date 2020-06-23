207d601c2dbee168bed67da0472fe13fc6a5c2fa
RichardPalotai <jr.richard.palotai@gmail.com>
2020-06-23 16:52:00 +0200

    Added Ruby Pickaxe and Italian language.

--- tutorialmod/src/main/java/com/ricsi/rubycraft/items/RubyPickaxe.java
+++ tutorialmod/src/main/java/com/ricsi/rubycraft/items/RubyPickaxe.java
@@ -2,19 +2,19 @@ package com.ricsi.rubycraft.items;
 
 import com.ricsi.rubycraft.rubycraft;
 import net.minecraft.item.IItemTier;
-import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.Item;
 import net.minecraftforge.common.ToolType;
 
 
-public class RubyPickaxe extends PickaxeItem {
+public class RubyPickaxe extends Item {
 
     public RubyPickaxe(IItemTier tier, int attackDamageIn, float attackSpeedIn, Properties builder) {
-        super(tier, 7, 10, builder
-            .maxDamage(7)
+        super(new Properties()
             .group(rubycraft.TAB)
             .addToolType(ToolType.PICKAXE, 10)
             .maxStackSize(1)
             .defaultMaxDamage(6)
+                .maxDamage(7)
         );
     }
 }
