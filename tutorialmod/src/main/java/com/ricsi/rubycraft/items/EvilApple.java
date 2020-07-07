ed748c31490f0c48030414405be5c344a315fd5c
RichardPalotai <jr.richard.palotai@gmail.com>
2020-07-07 13:56:33 +0200

    Added Ruby Pickaxe and Italian language.

--- tutorialmod/src/main/java/com/ricsi/rubycraft/items/EvilApple.java
+++ tutorialmod/src/main/java/com/ricsi/rubycraft/items/EvilApple.java
@@ -1,10 +1,18 @@
 package com.ricsi.rubycraft.items;
 
 import com.ricsi.rubycraft.rubycraft;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class EvilApple extends Item {
     public EvilApple() {
@@ -19,5 +27,10 @@ public class EvilApple extends Item {
                         ), 1.0f)
                         .build())
                 .group(rubycraft.TAB));
+    }
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+
+        tooltip.add(new StringTextComponent("Punishes bad persons!"));
     }
 }
